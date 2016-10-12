7a108f5900bbb84b3069d8148322ee689e926b7e
Mike Williamson <mike@korora.ca>
2016-10-11 21:41:09 -0400

    Initial commit

---
+++ test/yarntest_test.js
@@ -0,0 +1,9 @@
+import assert from 'assert'
+
+
+describe('Arithmetic', () => {
+  it('should calculate 1 + 1 correctly', () => {
+    const expectedResult = 2;
+    assert.equal(1+1, expectedResult)
+  });
+});
